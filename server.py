f7e0713c09d70831f6e98b23cc03714184218768
witchard <witchard@hotmail.co.uk>
2013-06-28 22:58:02 +0100

    First version, sliders are loaded and some ajax is called, but its not the right ajax or the right action and response...

---
+++ server.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import BaseHTTPServer, SimpleHTTPServer
+import json
+
+class ReqHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+  # Handle ordinary GET / HEAD requests in the static subfolder
+  def do_GET(self):
+    self.path = '/static' + self.path
+    SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+  def do_HEAD(self):
+    self.path = '/static' + self.path
+    SimpleHTTPServer.SimpleHTTPRequestHandler.do_HEAD(self)
+
+  # Handle posts only if they are json requests
+  def do_POST(self):
+    if self.headers['Content-Type'] == 'application/json':
+      self.data_string = self.rfile.read(int(self.headers['Content-Length']))
+      req = json.loads(self.data_string)
+      print req
+      resp = json.dumps( {'a': 5} )
+      self.send_response(200)
+      self.send_header("Content-Length", len(resp))
+      self.send_header("Content-Type", "application/json")
+      self.end_headers()
+      self.wfile.write(resp)
+    else:
+      self.send_response(400) 
+
+httpd = BaseHTTPServer.HTTPServer(('localhost', 8000), ReqHandler)
+httpd.serve_forever()
