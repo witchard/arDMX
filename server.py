0da1b79a100f52551996b70557c4114acdcb3ea2
witchard <witchard@hotmail.co.uk>
2016-08-10 22:26:58 +0100

    Added null backend for testing

--- server.py
+++ server.py
@@ -1,30 +1,45 @@
 #!/usr/bin/env python3
 from bottle import post, route, run, static_file, request
-import serial
 import sys
 import time
 
-# Connect to serial port
-port_file = open("comport.txt", "r")
-ser = None
-for line in port_file.readlines():
-  port = line.strip()
-  try:
+class ArduinoController():
+  def __init__(self):
+    import serial
+    port_file = open("comport.txt", "r")
+    ser = None
+    for line in port_file.readlines():
+      port = line.strip()
+      try:
+        if not ser:
+          print("Trying", port)
+          ser = serial.Serial(port, timeout=1)
+      except serial.serialutil.SerialException as e:
+        print("Serial port failure: " + str(e))
+    port_file.close()
     if not ser:
-      print("Trying", port)
-      ser = serial.Serial(port, timeout=1)
-  except serial.serialutil.SerialException as e:
-    print("Serial port failure: " + str(e))
-port_file.close()
-if not ser:
-  sys.exit(1)
-time.sleep(1)
-line = ser.readline()
-print(line.strip())
-while line != b"":
-  time.sleep(1)
-  line = ser.readline()
-  print(line.strip())
+      sys.exit(1)
+    time.sleep(1)
+    line = ser.readline()
+    print(line.strip())
+    while line != b"":
+      time.sleep(1)
+      line = ser.readline()
+      print(line.strip())
+    self.ser = ser
+
+  def __call__(self, values):
+    cmd = ""
+    for (chan, val) in req["lights"]:
+      cmd += "%sc%sw" % (chan, val)
+    self.ser.write(str.encode(cmd))
+
+class NullController():
+  def __call__(self, values):
+    for (chan, val) in values:
+      print( str(chan) + ' => ' + str(val) )
+
+ctrl = ArduinoController()
 
 @route('/')
 def index():
@@ -38,13 +53,7 @@ def serve_static(filename):
 def set():
   req = request.json
       
-  # Write it
-  cmd = ""
-  for (chan, val) in req["lights"]:
-    cmd += "%sc%sw" % (chan, val)
-  print(cmd)
-  ser.write(str.encode(cmd))
-  #ser.read()
+  ctrl(req['lights'])
 
   # Send back OK
   return {'ok': True}
